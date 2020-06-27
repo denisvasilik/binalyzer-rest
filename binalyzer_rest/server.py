906d83120768ad2e7c6614e12198f019bc6b7faf
Denis Vasilik <contact@denisvasilik.com>
2020-06-27 10:13:18 +0200

    Use DataProvider instead of BufferedIODataPRovider

--- binalyzer_rest/server.py
+++ binalyzer_rest/server.py
@@ -27,8 +27,8 @@ from flask import (
 from binalyzer import (
     Binalyzer,
     XMLTemplateParser,
-    SimpleTemplateProvider,
-    BufferedIODataProvider,
+    TemplateProvider,
+    DataProvider,
     __version__,
 )
 
@@ -88,22 +88,35 @@ def transform():
     destination_template_url = json_data['destination_template_url']
     data_urls = json_data['data_urls']
 
-    source_template = requests.get(source_template_url)
-    _destination_template = requests.get(destination_template_url)
+    source_template_response = requests.get(source_template_url)
+    destination_template_response = requests.get(destination_template_url)
 
     data_blobs = []
     for data_url in data_urls:
         data_blob = requests.get(data_url).content
         data_blobs.append(data_blob)
 
-    template = XMLTemplateParser(source_template.text).parse()
-    template_provider = SimpleTemplateProvider(template)
-    data_provider = BufferedIODataProvider(io.BytesIO(data_blobs[0]))
+    source_template = XMLTemplateParser(source_template_response.text).parse()
+    template_provider = TemplateProvider(source_template)
+    data_provider = DataProvider(io.BytesIO(data_blobs[0]))
     binalyzer = Binalyzer(template_provider, data_provider)
     binalyzer.template = template_provider.template
 
+    _destination_template = XMLTemplateParser(destination_template_response.text).parse()
+
+    # Use flow engine
+    # (1) Create annotated tree
+    #       Compare source and destination template tree
+    # (2)
+    # Traverse through destination tree
+    #   Check each item's existence in source tree
+    #       If it exists
+    #           Use bound data and apply destination template properties
+    #       Else
+    #           Fill template with dummy data
+
     return send_file(io.BytesIO(binalyzer.template.value),
-                     attachment_filename=binalyzer.template.id,
+                     attachment_filename=binalyzer.template.name,
                      mimetype='application/octet-stream')
 
 
