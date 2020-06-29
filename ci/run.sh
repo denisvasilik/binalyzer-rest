696c03cfb9a3f2c364ed40101aa32d77ffaa87f5
Denis Vasilik <contact@denisvasilik.com>
2020-06-29 19:49:40 +0200

    Fix package name

--- ci/run.sh
+++ ci/run.sh
@@ -5,17 +5,17 @@ set -e
 
 if [ ! -z "$TRAVIS_TAG" ]
 then
-    sed -i 's/__tag__ = ""/__tag__ = "'${TRAVIS_TAG}'"/g' binalyzer/__init__.py
+    sed -i 's/__tag__ = ""/__tag__ = "'${TRAVIS_TAG}'"/g' binalyzer_rest/__init__.py
     sed -i 's/__tag__ = ""/__tag__ = "'${TRAVIS_TAG}'"/g' setup.py
 fi
 
 if [ ! -z "$TRAVIS_BUILD_NUMBER" ]
 then
-    sed -i 's/__build__ = 0/__build__ = '${TRAVIS_BUILD_NUMBER}'/g' binalyzer/__init__.py
+    sed -i 's/__build__ = 0/__build__ = '${TRAVIS_BUILD_NUMBER}'/g' binalyzer_rest/__init__.py
     sed -i 's/__build__ = 0/__build__ = '${TRAVIS_BUILD_NUMBER}'/g' setup.py
 fi
 
 if [ ! -z "$TRAVIS_COMMIT" ]
 then
-    sed -i 's/__commit__ = \"00000000\"/__commit__ = \"'${TRAVIS_COMMIT::6}'\"/g' binalyzer/__init__.py
+    sed -i 's/__commit__ = \"00000000\"/__commit__ = \"'${TRAVIS_COMMIT::6}'\"/g' binalyzer_rest/__init__.py
 fi
