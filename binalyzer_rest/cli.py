dd299d2f73db4a79888a093c300d868ddbda24d0
Denis Vasilik <contact@denisvasilik.com>
2021-04-27 08:35:36 +0200

    Import functions from flask

--- binalyzer_rest/cli.py
+++ binalyzer_rest/cli.py
@@ -3,108 +3,20 @@
     ~~~~~~~~~~~~~~~~~~~
 
     CLI extension for the *binalyzer* command.
+
+    :copyright: 2021 Denis Vasilík
+    :license: MIT, see LICENSE for details.
 """
 import click
-import sys
-import ssl
-import os
-import logging
 
 from werkzeug.serving import run_simple
-from werkzeug.utils import import_string
-from flask._compat import text_type
+from flask.cli import (
+    CertParamType,
+    _validate_key,
+    show_server_banner,
+)
 
 from .rest import flask_app
-
-
-class CertParamType(click.ParamType):
-    """Click option type for the ``--cert`` option. Allows either an
-    existing file, the string ``'adhoc'``, or an import for a
-    :class:`~ssl.SSLContext` object.
-    """
-
-    name = 'path'
-
-    def __init__(self):
-        self.path_type = click.Path(
-            exists=True, dir_okay=False, resolve_path=True)
-
-    def convert(self, value, param, ctx):
-        try:
-            return self.path_type(value, param, ctx)
-        except click.BadParameter:
-            value = click.STRING(value, param, ctx).lower()
-
-            if value == 'adhoc':
-                try:
-                    import OpenSSL
-                except ImportError:
-                    raise click.BadParameter(
-                        'Using ad-hoc certificates requires pyOpenSSL.',
-                        ctx, param)
-
-                return value
-
-            obj = import_string(value, silent=True)
-
-            if sys.version_info < (2, 7, 9):
-                if obj:
-                    return obj
-            else:
-                if isinstance(obj, ssl.SSLContext):
-                    return obj
-
-            raise
-
-def _validate_key(ctx, param, value):
-    """The ``--key`` option must be specified when ``--cert`` is a file.
-    Modifies the ``cert`` param to be a ``(cert, key)`` pair if needed.
-    """
-    cert = ctx.params.get('cert')
-    is_adhoc = cert == 'adhoc'
-
-    if sys.version_info < (2, 7, 9):
-        is_context = cert and not isinstance(cert, (text_type, bytes))
-    else:
-        is_context = isinstance(cert, ssl.SSLContext)
-
-    if value is not None:
-        if is_adhoc:
-            raise click.BadParameter(
-                'When "--cert" is "adhoc", "--key" is not used.',
-                ctx, param)
-
-        if is_context:
-            raise click.BadParameter(
-                'When "--cert" is an SSLContext object, "--key is not used.',
-                ctx, param)
-
-        if not cert:
-            raise click.BadParameter(
-                '"--cert" must also be specified.',
-                ctx, param)
-
-        ctx.params['cert'] = cert, value
-
-    else:
-        if cert and not (is_adhoc or is_context):
-            raise click.BadParameter(
-                'Required when using "--cert".',
-                ctx, param)
-
-    return value
-
-def show_server_banner(env, debug):
-    """Show extra startup messages the first time the server is run,
-    ignoring the reloader.
-    """
-    if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
-        return
-
-    click.echo(' * Environment: {0}'.format(env))
-
-    if debug is not None:
-        click.echo(' * Debug mode: {0}'.format('on' if debug else 'off'))
 
 
 @click.command()
@@ -127,8 +39,7 @@ def show_server_banner(env, debug):
 @click.option('--with-threads/--without-threads', default=True,
               help='Enable or disable multithreading.')
 def rest(host, port, reload, debugger, with_threads, cert):
-    """Run a local test server. The reloader and debugger are enabled by default if
-    BINALYZER_ENV=development or BINALYZER_DEBUG=1.
+    """Run a local server (experimental).
     """
     show_server_banner('production', False)
 
